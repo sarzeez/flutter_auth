d5dd2018cd7204e9719b509b678b1468edd7423f
Air <air@sarzeez-2.local>
2023-12-21 20:28:05 +0500

    dio base config

--- lib/config/dio_config.dart
+++ lib/config/dio_config.dart
@@ -1,1 +1,9 @@
+import 'package:dio/dio.dart';
 
+Dio dio = Dio(
+  BaseOptions(
+    baseUrl: 'http://app.example.com',
+    connectTimeout: const Duration(seconds: 5),
+    receiveTimeout: const Duration(seconds: 5),
+  ),
+);
